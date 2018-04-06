fee61a720c204a3b2e0230c8edeb2669a590c442
brandon28139@gmail.com <khakiferret35>
2018-04-05 22:47:54 -0400

    New files

--- app/src/main/java/com/brandon28139apps/rutherfordcountyguide/MainActivity.java
+++ app/src/main/java/com/brandon28139apps/rutherfordcountyguide/MainActivity.java
@@ -1,5 +1,4 @@
 package com.brandon28139apps.rutherfordcountyguide;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -10,7 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 public class MainActivity extends AppCompatActivity
@@ -32,6 +32,12 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        WebView webView = (WebView) findViewById(R.id.whatsNewWebView);
+        webView.loadUrl("http://162.198.222.215/index.html");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
     }
 
     @Override
@@ -58,10 +64,6 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
